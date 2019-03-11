084097e1dce02fd5c1d228f5a9c4b5b0887afedc
omikhailk <13448403+omikhailk@users.noreply.github.com>
2019-03-11 13:48:02 +0300

    3. Refactor code into functions and add "if main"
    Refactor code into separate functions, add an "if main" block
    and add a URL validation function.
    
    Resolves: #4

--- comicscraper/main.py
+++ comicscraper/main.py
@@ -3,35 +3,90 @@
 from bs4 import BeautifulSoup
 import requests
 
-# Gets the source HTML for the 3rd Batman volume
-source = requests.get("https://dc.fandom.com/wiki/Batman_Vol_3").text
-soup = BeautifulSoup(source, "lxml")
+
+def main():
+    url = desired_user_url()
+
+    # Gets the source HTML for the user-inputted DC wiki url
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, "lxml")
+
+    list_of_issue_urls = get_issue_list_links(soup)
+    image_downloader(list_of_issue_urls)
 
 
-# Finds the anchor tags of the unordered list elements
-issue_list = soup.find("div", id="mw-content-text")
-issue_list = issue_list.find("ul").find_all("a")
+def desired_user_url():
+    """
+    Gets the user input for what URL they would like to visit
+    and get the comic cover images from.
+
+    It will then also validate the URL.
+
+    It then returns this URL as a string if it is valid, otherwise
+    it will repeat until it receives a valid URL.
+    """
+    while True:
+        input_msg = ("Please enter the URL for your comic page, it must be a"
+                     " DC Wiki page.\nURL: ")
+        url = input(input_msg)
+
+        request = requests.get(url)
+        if request.status_code == 200:
+            # Means that the URL exists
+            return url
+        # Otherwise the loop will keep on repeating until
+        # it receives a valid URL
+        print("\nYou did not enter a valid URL, please try again.\n\n")
+        continue
 
 
-# Constructs a list of URLs from the list of issues and their
-# anchor tags
-issue_urls = [anchorlink["href"] for anchorlink in issue_list]
-base_site_url = "https://dc.fandom.com"
-# Adds the base url of DC fandom to the individual issue
-# URLs so that the resulting URL is a valid one
-for i in range(0, len(issue_urls)):
-    issue_urls[i] = base_site_url + issue_urls[i]
+def get_issue_list_links(soup_object):
+    """
+    Takes in the predefined soup_object as input and then uses that
+    to do the tasks below
 
-# Loops through each URL and downloads the image from it
-for indiv_url in issue_urls:
-    indiv_url_source = requests.get(indiv_url).text
-    indiv_soup = BeautifulSoup(indiv_url_source, "lxml")
-    img_url = indiv_soup.find("a", class_="image image-thumbnail")["href"]
+    Finds the anchor tags of the unordered list elements.
+    Then construcsts a list of URLs from the list of issues and their
+    anchor tags.
 
-    requests_img_url = requests.get(img_url)
+    Then it adds the base URL of DC Wikia to the list of issue URLs.
 
-    issue_title = indiv_soup.title.string.split("|")[0].rstrip()
-    file_name = "_".join(issue_title.split())
+    It then returns these URLs as a list.
+    """
+    issue_list = soup_object.find("div", id="mw-content-text")
+    issue_list = issue_list.find("ul").find_all("a")
 
-    with open(f"covers/{file_name}.jpg", "wb") as f:
-        f.write(requests_img_url.content)
+    # Makes a list of the issue_urls
+    issue_urls = [anchorlink["href"] for anchorlink in issue_list]
+    # Adds the base URL to each element in the list of issue URLs
+    base_site_url = "https://dc.fandom.com"
+
+    for i in range(0, len(issue_urls)):
+        issue_urls[i] = base_site_url + issue_urls[i]
+
+    return issue_urls
+
+
+def image_downloader(issue_urls):
+    """
+    Loops through each URL in the list of article URLs,
+    then it downloads the image from each URLself.
+
+    The images are then saved in the covers/ directory.
+    """
+    for indiv_url in issue_urls:
+        indiv_url_source = requests.get(indiv_url).text
+        indiv_soup = BeautifulSoup(indiv_url_source, "lxml")
+        img_url = indiv_soup.find("a", class_="image image-thumbnail")["href"]
+
+        requests_img_url = requests.get(img_url)
+
+        issue_title = indiv_soup.title.string.split("|")[0].rstrip()
+        file_name = "_".join(issue_title.split())
+
+        with open(f"covers/{file_name}.jpg", "wb") as f:
+            f.write(requests_img_url.content)
+
+
+if __name__ == "__main__":
+    main()
